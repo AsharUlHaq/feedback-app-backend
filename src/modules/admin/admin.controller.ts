046afc39ebb3a75331f1642223ae6afd40f2d2a0
Ateeb Khan <ateeb@fynder.ai>
2024-07-02 20:19:05 +0500

    initial commit

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,10 +1,14 @@
-import { Handler } from "express";
+import type { Handler } from "express";
+import type { AdminTypes } from "./admin.schema";
 import { commonService } from "../common/common.service";
 import { LoggerService } from "../../utils/logger.util";
 import { userService } from "../user/user.service";
-import { NotFoundException } from "../../utils/exception.util";
-import { AdminTypes } from "./admin.schema";
+import {
+  ForbiddenException,
+  NotFoundException,
+} from "../../utils/exception.util";
 import { ResponseMapper } from "../../common/mapper/response.mapper";
+import { $Enums } from "@prisma/client";
 
 class AdminController {
   private readonly logger = LoggerService(AdminController.name);
@@ -14,6 +18,8 @@ class AdminController {
       const body = req.body as AdminTypes.ToggleUserStatus;
       const user = await userService.findOneById(body.id);
       if (!user) throw new NotFoundException("User not found");
+      if (user.role === $Enums.UserRole.ADMIN)
+        throw new ForbiddenException("Cannot change status of admin");
 
       await userService.update({
         data: { isActive: !user.isActive },
